c9065ad1bacfe785bc5e5513bc4fbfb13b8817e2
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-03-29 22:29:28 -0500

    Video

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -47,7 +47,7 @@
 
       figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/M-Iw1awJ1NU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     .row.mt-5
       h3 Modelo de datos
